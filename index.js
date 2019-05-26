20713578133ee6c509d99107bd56dd81f68d3e8c
Eduard Samuel <samueledi80@hotmail.com>
2019-05-26 15:16:53 +0200

    Working

--- index.js
+++ index.js
@@ -1,17 +1,96 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    document.querySelector(".tile").addEventListener("click", order());
-})
+let player = 1;
+let case1 = document.querySelector('#case1');
+let case2 = document.querySelector('#case2');
+let case3 = document.querySelector('#case3');
+let case4 = document.querySelector('#case4');
+let case5 = document.querySelector('#case5');
+let case6 = document.querySelector('#case6');
+let case7 = document.querySelector('#case7');
+let case8 = document.querySelector('#case8');
+let case9 = document.querySelector('#case9');
 
-let order = () => {
-    let xoro = ["x","o","x","o","x","o","x","o","x"];
-    for (let i = 0; i<xoro.length; i++){
-        if (i = xoro["x"]){
-            
-            console.log("its x")
-        } else if (i = "o"){
-            
-            console.log("its o")
-        }
-    
+
+
+case1.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case1.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case1.innerHTML = `<img src="img/o.png">`    
+    player = 1;
     }
-}
+});
+case2.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case2.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case2.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case3.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case3.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case3.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case4.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case4.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case4.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case5.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case5.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case5.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case6.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case6.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case6.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case7.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case7.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case7.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case8.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case8.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case8.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+case9.addEventListener("click", ()=> { 
+    if ( player == 1 ){
+    case9.innerHTML = `<img src="img/x.png">`
+    player = 0;
+    } else {
+    case9.innerHTML = `<img src="img/o.png">`    
+    player = 1;
+    }
+});
+
+
